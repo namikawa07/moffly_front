5da403df2aef4354e5a1c25d401445c493416273
namikawa07 <73113536+namikawa07@users.noreply.github.com>
2022-12-30 14:32:50 +0900

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <button
+      type="button"
+      name="button"
+      @click="getMsg"
+      class="button"
+    >
+      RailsからAPIを取得する
+    </button>
+    <div
+    >
+      {{ hello.hello_string }}
+      {{ hello.hello_text }}
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  data () {
+    return {
+      msgs: []
+    }
+  },
+   computed: {
+    ...mapState('hello', ['hello']),
+  },
+  methods: {
+    getMsg () {
+      this.$store.dispatch('hello/fetch')
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.button{
+  color: red;
+}
+</style>
